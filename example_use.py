68412edbd5074859be4f04383924104fadf80328
wiktor <w.rapacz@protonmail.com>
2024-01-14 11:11:15 +0100

    impleneting random graph optimization and visualization

--- example_use.py
+++ example_use.py
@@ -10,6 +10,13 @@ nodes_set, edges_set = graph_generator.generate_graph()
 # Visualization of random generated graph
 graph_visualizer = GraphVisualizer(nodes_set, edges_set)
 graph_visualizer.visualize_graph()
+# Optimizing random graph using ACO
+random_graph_map = GraphMapCreator(nodes_set, edges_set, 'Q1', 'O1')
+aco_optimizer = AntColonyOptimizer(random_graph_map, euclidean)
+optimized_nodes, optimized_neighbors = aco_optimizer.solve_aco()
+# Visualization of optimal path for random graph
+graph_visualizer = GraphVisualizer(optimized_nodes, optimized_neighbors)
+graph_visualizer.visualize_graph()
 # Visualization of provided data
 graph_visualizer = GraphVisualizer(nodes_data, neighbors_data, 10, 10, 70)
 graph_visualizer.visualize_graph()
